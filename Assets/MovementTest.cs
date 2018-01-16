c0e431fff06349f1e6baa92ca23c1ab4a5239d1d
LOKI <LOKI>
2018-01-16 14:27:08 -0200

    Fazendo testes com o carro;

---
+++ Assets/MovementTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+
+    public float speed = 10f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical") * speed;
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        rb.AddForce(new Vector3(horizontal, 0f, vertical));
+    }
+}
