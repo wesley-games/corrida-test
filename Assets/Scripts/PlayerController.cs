33a56d0a62b5682e0f9461b2c335b556b1815a73
wesleyallanrocha <wesleyallanrocha@gmail.com>
2018-05-21 00:22:15 -0300

    Tentando recomeçar o projeto;

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // public float speed = 10;
+
+    // private Vector3 velocity;
+    // private Rigidbody body;
+
+    // void Start()
+    // {
+    //     body = GetComponent<Rigidbody>();
+    // }
+
+    // void Update()
+    // {
+    //     float horizontal = Input.GetAxis("Horizontal");
+    //     float vertical = Input.GetAxis("Vertical");
+    //     velocity = new Vector3(horizontal, 0, vertical) * speed;
+    // }
+
+    // void FixedUpdate()
+    // {
+    //     body.velocity = velocity;
+    // }
+}
